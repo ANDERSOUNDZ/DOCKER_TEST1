8dd9f244278c9161f4ec6508b2b5e001b84e793a
ANDERSOUNDZ <andersonmikol@live.com>
2026-02-06 00:47:28 -0500

    Refactorizacion Frontend 1.2: Funcionalidad Basica: Paginacion Cliente

--- frontend_infrastructure/src/app/core/services/state/app-state.service.ts
+++ frontend_infrastructure/src/app/core/services/state/app-state.service.ts
@@ -1,11 +1,28 @@
 import { Injectable, signal } from '@angular/core';
 import { Cliente } from '../../../domain/models/cliente/cliente.model';
 
+export interface PaginationState {
+  currentPage: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+
 @Injectable({ providedIn: 'root' })
 export class AppStateService {
   private _clientes = signal<Cliente[]>([]);
   private _loading = signal<boolean>(false);
   private _searchQuery = signal<string>('');
+
+  private _pagination = signal<PaginationState>({
+    currentPage: 0,
+    pageSize: 10,
+    totalElements: 0,
+    totalPages: 0
+  });
+
+  public pagination = this._pagination.asReadonly();
 
   public clientes = this._clientes.asReadonly();
   public isLoading = this._loading.asReadonly();
@@ -21,5 +38,13 @@ export class AppStateService {
 
   setSearchQuery(query: string) {
     this._searchQuery.set(query);
+  }
+
+  setPagination(data: PaginationState) {
+    this._pagination.set(data);
+  }
+
+  setPage(page: number) {
+    this._pagination.update(prev => ({ ...prev, currentPage: page }));
   }
 }
--- frontend_infrastructure/src/app/infrastructure/ui/modules/cliente/cliente-list/cliente-list.ts
+++ frontend_infrastructure/src/app/infrastructure/ui/modules/cliente/cliente-list/cliente-list.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClienteRepository } from '../../../../../domain/repository/cliente/cliente.repository';
 import { AppStateService } from '../../../../../core/services/state/app-state.service';
-import { ClienteForm } from "../cliente-form/cliente-form";
+import { ClienteForm } from '../cliente-form/cliente-form';
 
 @Component({
   selector: 'app-cliente-list',
@@ -20,13 +20,26 @@ export class ClienteList implements OnInit {
     this.loadClientes(this.state.searchQuery());
   }
 
-  loadClientes(search: string = ''): void {
+  loadClientes(
+    search: string = this.state.searchQuery(),
+    page: number = this.state.pagination().currentPage,
+  ): void {
     this.state.setLoading(true);
     this.state.setSearchQuery(search);
+    this.state.setPage(page); // Persistimos la página actual
 
-    this.clienteRepo.getAll(search, 0, 10).subscribe({
+    this.clienteRepo.getAll(search, page, this.state.pagination().pageSize).subscribe({
       next: (res) => {
         this.state.setClientes(res.data.content);
+
+        // Actualizamos los metadatos de paginación con lo que envió Spring
+        this.state.setPagination({
+          currentPage: res.data.number,
+          pageSize: res.data.size,
+          totalElements: res.data.totalElements,
+          totalPages: res.data.totalPages,
+        });
+
         this.state.setLoading(false);
       },
       error: () => this.state.setLoading(false),
@@ -43,5 +56,11 @@ export class ClienteList implements OnInit {
   }
   closeForm() {
     this.showForm = false;
+  }
+
+  goToPage(page: number): void {
+    if (page >= 0 && page < this.state.pagination().totalPages) {
+      this.loadClientes(this.state.searchQuery(), page);
+    }
   }
 }
